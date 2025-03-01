03c7e68ffbe30d26db977210f5fb0e7117e10f96
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-02 02:02:22 +0900

    feature: Add NewStage constructor for Stage value object with validation

---
+++ backend/internal/task/application/domain/stage.go
@@ -0,0 +1,36 @@
+package domain
+
+import "errors"
+
+const (
+	StageTodo = "TODO"
+	StageInProgress = "IN_PROGRESS"
+	StageDone = "Done"
+)
+
+type Stage struct {
+	value string
+}
+
+func NewStage(stage string) (Stage, error) {
+	if err := validateStage(stage); err != nil {
+		return Stage{}, err
+	}
+
+	return Stage{value:  stage}, nil
+}
+
+func validateStage(stage string) error {
+	switch stage {
+	case StageTodo, StageInProgress, StageDone:
+		return nil
+	case "":
+		return errors.New("stage must not be empty")
+	default:
+		return errors.New("unexpected stage value")
+	}
+}
+
+func (s Stage) Value() string {
+	return s.value
+}

da6d4b481f3217507f3202eb6bd740aeefa840c6
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-01 23:40:21 +0900

    feat: Add validation for Title value object

--- backend/internal/task/application/domain/title.go
+++ backend/internal/task/application/domain/title.go
@@ -1,11 +1,36 @@
 package domain
 
+import (
+	"errors"
+	"fmt"
+)
+
+const MAX_TITLE_LENGTH = 20
+
 type Title struct {
 	value string
 }
 
 func NewTitle(title string) (Title, error) {
+	err := validate(title)
+	if err != nil {
+		return Title{}, err
+	}
+
+
 	return Title{value: title}, nil
+}
+
+func validate(title string) error {
+	if title == "" {
+		return errors.New("title must not be empty")
+	}
+
+	if len(title) > MAX_TITLE_LENGTH {
+		return fmt.Errorf(fmt.Sprintf("title must be at most %d characters", MAX_TITLE_LENGTH))
+	}
+
+	return nil
 }
 
 func (t *Title) Value() string {

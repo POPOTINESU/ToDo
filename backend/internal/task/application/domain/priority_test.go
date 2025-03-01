8a07af87108dc026e7388ec52d01f76f7f8c6a67
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-02 00:40:17 +0900

    test: Add test for successful Priority creation

---
+++ backend/internal/task/application/domain/priority_test.go
@@ -0,0 +1,38 @@
+package domain_test
+
+import (
+	"ToDo/internal/task/application/domain"
+	"testing"
+)
+
+func TestNewPriority(t *testing.T) {
+	data := []struct {
+		testName string
+		value string
+		expected string
+		errMsg string
+	}  {
+		{"create low priority object", "low", "low", ""},
+		{"create middle priority object", "low", "low", ""},
+		{"create high object", "high", "high", ""},
+	}
+
+	for _, d := range data {
+		t.Run(d.testName, func(t *testing.T) {
+			result, err := domain.NewPriority(d.value)
+
+			if d.expected != result.Value() {
+				t.Errorf("期待する結果 %s, 得られた結果 %s", d.expected, result.Value())
+			}
+
+			var errMsg string
+			if err != nil {
+				errMsg = err.Error()
+			}
+
+			if d.errMsg != errMsg {
+				t.Errorf("予想されたエラーメッセージ %s, 得られたメッセージ %s", d.errMsg, errMsg)
+			}
+		})
+	}
+}

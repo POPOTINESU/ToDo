da4a19365996ba3d08a451ce65e82bbe97474b29
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-02 05:18:32 +0900

    feat: Add input port for create task usecase

---
+++ backend/internal/task/application/port/in/create_task.go
@@ -0,0 +1,18 @@
+package in
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type CreateTaskDTO struct {
+	Title string `json:"title"`
+	Description string `json:"description"`
+	Priority string `json:"priority"`
+	Stage string `json:"stage"`
+}
+
+type CreateTaskUseCase interface {
+	Execute(ctx context.Context, dto *CreateTaskDTO) (uuid.UUID, error)
+}
